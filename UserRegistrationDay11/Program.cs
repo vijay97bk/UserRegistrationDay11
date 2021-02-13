de21ed13120215d144113ea52530776acb6fc8d5
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-13 09:29:59 +0530

    [vijay]firdtCommit

---
+++ UserRegistrationDay11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationDay11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome message");
+        }
+    }
+}
